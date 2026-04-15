43719ac47dc9961e60741b612dadce1ab5f8278c
SrijanG05 <gsrijan777@gmail.com>
2026-04-15 12:25:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
     //Write your code here
-
+	 const form = document.getElementById('form1');
+    const firstName = form.elements['fname'].value.trim();
+    const lastName = form.elements['lname'].value.trim();
+    if (firstName === "" && lastName === "") {
+        alert("Please enter your name.");
+    } else {
+            alert(`${firstName} ${lastName}`);
+    }
 }
